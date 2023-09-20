5ee8fe623276f548d4147bc365dfc30fda43216a
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-09-20 14:36:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const formElement = document.querySelector('form');
+
+const inputButton = formElement.querySelector('input[type="button"]');
+
+inputButton.addEventListener("click",function () {
+option_remove();	
+});
+
+function option_remove() {
+	const selected_element = document.getElementById('colorSelect');
+
+	const selected_options = selected_element.options[selected_element.selectedIndex];
+
+	if(selected_options){
+		selected_element.removeChild(selected_options);
+	}
+}
